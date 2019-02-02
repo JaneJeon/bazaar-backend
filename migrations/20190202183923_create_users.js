8fcf7f74a7c36e720ac48b97c28a2899aee9b104
Jane Jeon <JaneJeon9719@gmail.com>
2019-02-02 18:46:03 -0500

    migration

---
+++ migrations/20190202183923_create_users.js
@@ -0,0 +1,23 @@
+const tableName = require("pluralize")("user")
+
+exports.up = knex =>
+  knex.schema.createTable(tableName, table => {
+    table.increments()
+    table
+      .text("email")
+      .notNullable()
+      .unique()
+    table
+      .text("username")
+      .notNullable()
+      .unique()
+    table.text("password").notNullable()
+    table
+      .boolean("deleted")
+      .notNullable()
+      .defaultTo(false)
+
+    table.timestamps(true, true)
+  })
+
+exports.down = knex => knex.schema.dropTable(tableName)
