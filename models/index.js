1024d3befab460326427eaef9f60be3df5fce22e
Jane Jeon <JaneJeon9719@gmail.com>
2019-02-05 21:47:19 -0500

    closes #37

--- models/index.js
+++ models/index.js
@@ -2,7 +2,8 @@ const { Model } = require("objection")
 const knex = require("knex")
 const knexfile = require("../config/database")
 const User = require("./user")
+const Picture = require("./picture")
 
 Model.knex(knex(knexfile)) // yo dawg
 
-module.exports = { User }
+module.exports = { User, Picture }
