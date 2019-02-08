ef7cb9c68615debf5a98c3bdb3d07ea5424297e3
Jane Jeon <JaneJeon9719@gmail.com>
2019-02-07 19:18:37 -0500

    closes #49

--- config/multer.js
+++ config/multer.js
@@ -1,8 +1,7 @@
 const multer = require("multer")
-const multerS3 = require("multer-s3")
-const s3 = require("./s3")
+const storage = multer.memoryStorage()
 
-// read https://github.com/badunk/multer-s3#readme
 module.exports = multer({
-  storage: multerS3({ s3, bucket: process.env.PICTURE_BUCKET })
+  storage,
+  limits: { fileSize: process.env.IMAGE_MAX_FILESIZE }
 })
