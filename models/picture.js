5a750036323746ed4e1637621bebbb6a0df91b26
Jane Jeon <JaneJeon9719@gmail.com>
2019-02-08 05:49:57 -0500

    Improved file upload

--- models/picture.js
+++ models/picture.js
@@ -1,4 +1,7 @@
 const BaseModel = require("./base")
+const sharp = require("sharp")
+const s3 = require("../config/s3")
+const uuid = require("uuid/v1")
 
 class Picture extends BaseModel {
   static get jsonSchema() {
@@ -19,6 +22,29 @@ class Picture extends BaseModel {
       required: ["title", "url"],
       additionalProperties: false
     }
+  }
+
+  async $beforeInsert(queryContext) {
+    await super.$beforeInsert(queryContext)
+
+    const image = await sharp(url)
+      .resize(process.env.IMAGE_MAX_WIDTH, process.env.IMAGE_MAX_HEIGHT, {
+        withoutEnlargement: true,
+        fit: "inside"
+      })
+      .jpeg({ quality: 90 })
+      .toBuffer()
+
+    const file = await s3
+      .upload({
+        Key: `${uuid()}.jpeg`,
+        Body: image,
+        Bucket: process.env.PICTURE_BUCKET,
+        ACL: "public-read"
+      })
+      .promise()
+
+    this.url = file.Location
   }
 }
 
