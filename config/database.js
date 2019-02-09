80471b9266b1b673aaec535f0dd0bec7f6ae333f
Jane Jeon <JaneJeon9719@gmail.com>
2019-02-08 21:21:04 -0500

    seeding complete, and picture model fixed

--- config/database.js
+++ config/database.js
@@ -5,7 +5,6 @@ module.exports = {
   client: "pg",
   connection: process.env.DATABASE_URL,
   debug: (process.env.DEBUG || "").includes("knex"),
-  migrations: {
-    directory: join(__dirname, "..", "migrations")
-  }
+  migrations: { directory: join(__dirname, "..", "migrations") },
+  seeds: { directory: join(__dirname, "..", "seeds") }
 }
