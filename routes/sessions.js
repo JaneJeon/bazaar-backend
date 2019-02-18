473cb6d14f78b77bf46ae7f4ca443dce969a3c29
Jane Jeon <JaneJeon9719@gmail.com>
2019-02-18 16:02:45 -0500

    logging in sends user info

--- routes/sessions.js
+++ routes/sessions.js
@@ -3,7 +3,9 @@ const passport = require("passport")
 
 module.exports = Router()
   // CREATE session
-  .post("/", passport.authenticate("local"), (req, res) => res.sendStatus(201))
+  .post("/", passport.authenticate("local"), (req, res) =>
+    res.status(201).send(req.user)
+  )
   // DELETE session
   .delete("/", (req, res) => {
     req.logout()
