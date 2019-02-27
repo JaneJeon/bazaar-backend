0bb40ceffa38dc64f8859e2e05ab1c51cb1ad741
Jane Jeon <JaneJeon9719@gmail.com>
2019-02-27 04:16:42 -0500

    closes #75

---
+++ models/token.js
@@ -0,0 +1,23 @@
+const redis = require("../config/redis")
+const { sync: uid } = require("uid-safe")
+
+exports.generate = async (prefix, key, value = 1, expire = 86400) => {
+  const token = `${key}:${uid(24)}`
+  await redis.setex(`${prefix}:${token}`, expire, value)
+
+  return token
+}
+
+exports.fetch = async (prefix, token) => redis.get(`${prefix}:${token}`)
+
+exports.consume = (prefix, key) =>
+  new Promise(resolve => {
+    const stream = redis.scanStream({ match: `${prefix}:${key}:*` })
+    stream.on("data", tokens => {
+      if (tokens.length) {
+        stream.pause()
+        redis.unlink(tokens).then(stream.resume)
+      }
+    })
+    stream.on("end", resolve)
+  })
