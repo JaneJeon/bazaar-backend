7b6a211013c21810a41dcd7ca375da4e9c420110
Jane Jeon <JaneJeon9719@gmail.com>
2019-02-28 15:22:30 -0500

    database url fixed...

--- config/database.js
+++ config/database.js
@@ -2,10 +2,7 @@ const { join } = require("path")
 
 module.exports = {
   client: "pg",
-  connection: {
-    url: process.env.DATABASE_URL,
-    timezone: "UTC"
-  },
+  connection: process.env.DATABASE_URL,
   debug: (process.env.DEBUG || "").includes("knex"),
   migrations: { directory: join(__dirname, "..", "migrations") },
   seeds: { directory: join(__dirname, "..", "seeds") }
