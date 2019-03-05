094096faf383ec38f7396e43a2fdcd6c43776354
Jane Jeon <JaneJeon9719@gmail.com>
2019-03-05 18:49:17 -0500

    fix environment loading (albeit fugly)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv-defaults").config()
 require("express-async-errors")
 require("./config/passport")
 
--- config/database.js
+++ config/database.js
@@ -1,4 +1,9 @@
 const { join } = require("path")
+if (!process.env.NODE_ENV)
+  require("dotenv-defaults").config({
+    path: join(__dirname, "..", ".env"),
+    defaults: join(__dirname, "..", ".env.defaults")
+  })
 
 module.exports = {
   client: "pg",
