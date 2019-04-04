d78064ea2cab880ad2a0ce756a021168fe891966
Jane Jeon <JaneJeon9719@gmail.com>
2019-04-04 17:22:26 -0400

    closes #193

--- migrations/6-stripify.js
+++ migrations/6-stripify.js
@@ -1,14 +1,13 @@
-// add stripe_customer_id to users table for buyers, stripe_account_id for artists
-// stripe_charge_id for payments table
+// TODO: stripe_charge_id for payments table
 const tableName = "users"
 
 exports.up = knex =>
   knex.schema.table(tableName, table => {
     table.text("stripe_account_id")
-    table.text("stripe_charge_id")
+    table.text("stripe_customer_id")
   })
 
 exports.down = knex =>
   knex.schema.table(tableName, table => {
-    table.dropColumns(["stripe_account_id", "stripe_charge_id"])
+    table.dropColumns(["stripe_account_id", "stripe_customer_id"])
   })
