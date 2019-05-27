15e877f1b4d0d4a807d25250e30cbf79e42f1e07
RyanHallDartmouth <46539276+RyanHallDartmouth@users.noreply.github.com>
2019-05-26 22:02:54 -0400

    fixes favorites tests

--- test/routes/favorites.js
+++ test/routes/favorites.js
@@ -2,17 +2,17 @@ const request = require("supertest")(require("../../app"))
 const { users } = require("./tokens")
 const redis = require("../../lib/redis")
 
-describe.skip("favorites routes", () => {
-  let arts
+describe("favorites routes", () => {
+  let arts, token
 
   before(async () => {
     pictures = await redis.smembers("pictures")
-    const res = await request.get("/arts").expect(200)
-    arts = res.body
-  })
 
-  beforeEach(async () => {
-    await request.post("/sessions").send(users[0])
+    const response = await request.get("/arts").expect(200)
+    arts = response.body
+
+    const res = await request.post("/tokens").send(users[0])
+    token = res.body
   })
 
   describe("GET /users/:userId/favorites", () => {
@@ -28,9 +28,9 @@ describe.skip("favorites routes", () => {
   })
 
   describe("GET /arts/:artId/favorites", () => {
-    it("should list favorites by an art", async () => {
+    it("should list favorites by an art", async () =>
       await request.get(`/arts/${arts[0].id}/favorites`).expect(200)
-    })
+    )
   })
 
   describe("POST /arts/:artId/favorites", () => {
@@ -45,6 +45,7 @@ describe.skip("favorites routes", () => {
         it("should like art", async () => {
           const res = await request
             .post(`/arts/${arts[0].id}/favorites`)
+            .set("Authorization", "Bearer " + token)
             .expect(201)
         })
       })
@@ -63,6 +64,7 @@ describe.skip("favorites routes", () => {
         it("should remove favorite", async () => {
           const res = await request
             .delete(`/arts/${arts[0].id}/favorites`)
+            .set("Authorization", "Bearer " + token)
             .expect(204)
         })
       })
